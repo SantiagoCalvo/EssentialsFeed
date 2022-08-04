38da986498835282f66e7f7343f8cb791474f0b0
SantiagoCalvo <s.calvo11@uniandes.edu.co>
2022-08-03 19:57:17 -0500

    create the FeedItem Model

---
+++ EssentialsFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialsFeed
+//
+//  Created by santiago calvo on 3/08/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
