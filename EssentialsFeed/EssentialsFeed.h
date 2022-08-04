c41d048d519f3650c08ee4cf7fd0665462324a67
SantiagoCalvo <s.calvo11@uniandes.edu.co>
2022-08-03 19:51:38 -0500

    Initial Commit

---
+++ EssentialsFeed/EssentialsFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentialsFeed.h
+//  EssentialsFeed
+//
+//  Created by santiago calvo on 3/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialsFeed.
+FOUNDATION_EXPORT double EssentialsFeedVersionNumber;
+
+//! Project version string for EssentialsFeed.
+FOUNDATION_EXPORT const unsigned char EssentialsFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialsFeed/PublicHeader.h>
+
+
